c12d0f4242ca95c5ce256ff228eb5d0432d5415b
Weilu Jia <optix2000@teitoku.net>
2016-09-04 02:27:35 -0700

    Keep original setSkin function

---
+++ packlist.js
@@ -0,0 +1,26 @@
+/**
+ * XDCC Parser
+ * |- Javascript Module
+ *
+ * This software is free software and you are permitted to
+ * modify and redistribute it under the terms of the GNU General
+ * Public License version 3 as published by the Free Sofware
+ * Foundation.
+ *
+ * @link http://xdccparser.is-fabulo.us/
+ * @version 1.2.0
+ * @author Alex 'xshadowfire' Yu <ayu@xshadowfire.net>
+ * @author DrX
+ * @copyright 2008-2009 Alex Yu and DrX
+ */
+
+function p() {
+	this.setSkin=function(id) {
+		document.getElementById("skin").href = "style"+id+".css";
+		var exdate=new Date();
+		exdate.setDate(exdate.getDate()+7);
+		document.cookie="skin"+"="+escape(id)+";expires="+exdate.toGMTString();
+	};
+}
+
+var p = new p();
