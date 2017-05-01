67ab8314c38820da0d36808f7da0b83c1283acb6
Weilu Jia <optix2000@gmail.com>
2017-04-30 17:11:49 -0700

    Objectify new code for better separation

--- js/packlist-ng.js
+++ js/packlist-ng.js
@@ -1,49 +1,55 @@
-otree = new XML.ObjTree()
-xhttp = new XMLHttpRequest()
-var botdata;
-var converted_packlist = [];
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        botdata = otree.parseXML(this.responseText)
-        packlist = botdata['iroffer']['packlist']['pack']
-        for (i=0; i<packlist.length; i++) {
-            var tmp = {};
-            var pack = packlist[i];
-            tmp['s'] = Math.floor((parseInt(pack['packbytes'])/1024)/1024);
-            tmp['b'] = 'Teacup';
-            tmp['n'] = parseInt(pack['packnr']);
-            tmp['f'] = pack['packname'];
-            converted_packlist.push(tmp);
+var packlist = function() {
+    this.converted_packlist = [];
+
+    this.fetch_bot = function(name, url) {
+        var otree = new XML.ObjTree();
+        var xhttp = new XMLHttpRequest();
+        var botdata;
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                botdata = otree.parseXML(xhttp.responseText);
+                packlist = botdata['iroffer']['packlist']['pack'];
+                for (i=0; i<packlist.length; i++) {
+                    var tmp = {};
+                    var pack = packlist[i];
+                    tmp['s'] = Math.floor((parseInt(pack['packbytes'])/1024)/1024);
+                    tmp['b'] = name;
+                    tmp['n'] = parseInt(pack['packnr']);
+                    tmp['f'] = pack['packname'];
+                    this.converted_packlist.push(tmp);
+                };
+                p.k = this.converted_packlist;
+                p.flush();
+            }
+        }
+        xhttp.open('GET', url, true);
+        xhttp.send();
+    }
+    this.dosearch = function() {
+        var search = document.getElementById('search').value.toLowerCase();
+        console.log(search);
+        var search_list = [];
+        if (search != '') {
+            for (i=0; i<converted_packlist.length; i++) {
+                pack = converted_packlist[i];
+                if (pack['f'].toLowerCase().includes(search)) {
+                    search_list.push(pack);
+                    console.log(pack);
+                };
+            };
+            p.k = search_list;
+        } else {
+            p.k = converted_packlist;
+            console.log('nope');
         };
-        p.k = converted_packlist;
         p.flush();
     }
-}
-xhttp.open('GET', 'bot.xml', true);
-
-dosearch = function() {
-    var search = document.getElementById('search').value.toLowerCase();
-    console.log(search);
-    var search_list = [];
-    if (search != '') {
-        for (i=0; i<converted_packlist.length; i++) {
-            pack = converted_packlist[i];
-            if (pack['f'].toLowerCase().includes(search)) {
-                search_list.push(pack);
-                console.log(pack);
-            };
-        };
-        p.k = search_list;
-    } else {
-        p.k = converted_packlist;
-        console.log('nope');
-    };
-    p.flush();
-}
 
 
-init = function() {
-    console.log('init');
-    p.init();
-    xhttp.send();
+    this.init = function() {
+        console.log('init');
+        p.init();
+        this.fetch_bot();
+    }
 }
+var pl = new packlist();
